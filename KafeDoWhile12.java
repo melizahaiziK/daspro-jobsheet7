4781cc794a2254e8571a569b6afd3d9b21e503db
melizahaiziK <melizaha1zik@gmail.com>
2025-10-24 14:19:30 +0700

    KafeDoWhile.java

---
+++ KafeDoWhile12.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class KafeDoWhile12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int kopi;
+        int teh;
+        int roti;
+        int hargaKopi = 12000;
+        int hargaTeh = 7000;
+        int hargaRoti = 20000;
+        int totalHarga;
+
+        do {
+           System.out.print("Masukkan nama pelanggan (ketik 'batal' untuk keluar): ");
+           String namaPelanggan = sc.nextLine();
+            if (namaPelanggan.equalsIgnoreCase("batal")) {
+                System.out.println("Transaksi dibatalkan.");
+                System.out.println("Semua transaksi selesai");
+                break;
+            }
+            System.out.print("Jumlah kopi: ");
+            kopi = sc.nextInt();
+            System.out.print("Jumlah teh: ");
+            teh = sc.nextInt();
+            System.out.print("Jumlah roti: ");
+            roti = sc.nextInt();
+
+            totalHarga = (kopi * hargaKopi) + (teh * hargaTeh) + (roti * hargaRoti);
+            System.out.println("Total yang harus dibayar: Rp" + totalHarga);
+            sc.nextLine();
+        }  while (true);
+
+    }
+}
