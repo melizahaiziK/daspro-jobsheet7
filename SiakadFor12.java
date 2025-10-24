5a1cd36b189c0a6d2233d37672803cf1a373eb8c
melizahaiziK <melizaha1zik@gmail.com>
2025-10-24 11:31:36 +0700

    modifikasi percobaan 1

--- SiakadFor12.java
+++ SiakadFor12.java
@@ -3,6 +3,9 @@ public class SiakadFor12 {
    public static void main(String[] args) {
    Scanner sc = new Scanner(System.in);
     double nilai, tertinggi = 0, terendah = 100;
+    int lulus = 0;
+    int tidakLulus = 0;
+
     for (int i = 1; i <= 10; i++) {
         System.out.print("Masukkan niai mahasiswa ke-" + i + ": ");
         nilai = sc.nextDouble();
@@ -12,9 +15,13 @@ public class SiakadFor12 {
         if (nilai < terendah) {
             terendah = nilai;
         }
+         if (nilai >= 60) lulus++;
+            else tidakLulus++;
     }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa tidak lulus: " + tidakLulus);
 
    } 
 }
